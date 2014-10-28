357d8a8b2375cb05e5135c70564c9efca84ba4fc
jarppe <jarppe@metosin.fi>
2014-10-28 20:44:01 +0200

    ring tests

---
+++ test/clj/ring_test.clj
@@ -0,0 +1,15 @@
+(ns ring-test
+  (:require [midje.sweet :refer :all]
+            [ring :refer :all]))
+
+(fact
+  (let [handler (fn [req] {:status 200})
+        app     (frame-options handler)
+        resp    (app {})]
+    resp => {:status 200
+             :headers {"x-frame-options" "SAMEORIGIN"}}))
+
+(fact
+  (let [app (frame-options (constantly {:status 200}))]
+    (app {}) => {:status 200
+                 :headers {"x-frame-options" "SAMEORIGIN"}}))
