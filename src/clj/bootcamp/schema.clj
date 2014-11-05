1e1dd71e846ae199432d010a875f92186012b540
jarppe <jarppe@metosin.fi>
2014-11-05 11:30:54 +0200

    renamed schema.core ns alias

--- src/clj/bootcamp/schema.clj
+++ src/clj/bootcamp/schema.clj
@@ -1,28 +1,28 @@
 (ns bootcamp.schema
-  (:require [schema.core :as s]
-            [schema.coerce :as sc]))
+  (:require [schema.core :as sc]
+            [schema.coerce :as scoerce]))
 
 ;; Schemas - https://github.com/Prismatic/schema
 
 ; a schema can be
 
 ; a Class
-(s/check Long "foo")
-(s/check java.util.Date "bar")
+(sc/check Long "foo")
+(sc/check java.util.Date "bar")
 
 ; Validate throws
-(s/validate Long 5)
+(sc/validate Long 5)
 
 ; a collection or map containing Schemas
-(s/check [String] ["a" 5 "b"])
+(sc/check [String] ["a" 5 "b"])
 
-(s/check {:a Long
-          :b String}
-         {:a :error
-          :b "foo"})
+(sc/check {:a Long
+           :b String}
+          {:a :error
+           :b "foo"})
 
 ; schema.core ns has loads of helpers / aliases
-(s/check s/Int 5)
+(sc/check sc/Int 5)
 
 ; Those are especially important if developing for Clj+Cljs!
 ; (def Inst
@@ -30,28 +30,30 @@
 ;   #+clj java.util.Date #+cljs js/Date)
 
 ; predicates
-(s/check (s/pred (fn [v] (= v "foobar")) 'not-foobar) "barfoo")
+(sc/check (sc/pred (fn [v] (= v "foobar")) 'not-foobar) "barfoo")
 
 ; Something which implements Schema-protocol.
 ; Schema.core provides multiple utilities:
-(s/check (s/eq "foobar") "barfoo")
-(s/check (s/maybe s/Int) nil)
-(s/check (s/either s/Str s/Int) "foo")
-(s/check (s/both s/Int (s/pred even? 'not-even)) 5)
-(s/check (s/enum :foo :bar) :asd)
+(sc/check (sc/eq "foobar") "barfoo")
+(sc/check (sc/maybe sc/Int) nil)
+(sc/check (sc/either sc/Str sc/Int) "foo")
+(sc/check (sc/both sc/Int (sc/pred even? 'not-even)) 5)
+(sc/check (sc/enum :foo :bar) :asd)
 
 ;; EXCERCISES:
 
 ; Fix these
-(s/defschema Author {:fname s/Str})
+(sc/defschema Author {:fname sc/Str
+                      :lname sc/Str})
 
-(s/defschema Book {:name s/Str
-                   :langs s/Str
-                   :pages s/Str})
+(sc/defschema Book {:name   sc/Str
+                    :langs  #{sc/Keyword}
+                    :pages  sc/Int
+                    :authors [sc/Keyword]})
 
 ; More, coercions:
-(defn str->keyword-matcher[schema]
-  (if (= schema s/Keyword)
+(defn str->keyword-matcher [schema]
+  (if (= schema sc/Keyword)
     (fn [v]
       ; Fix this
       (if (string? v) v v))))
@@ -62,10 +64,7 @@
     ; Add another matcher here, to turn vectors into sets
     ))
 
-; FIXME: Not usable with mongo get-book
-; MongoBook
-(defn coerce-book [json-book]
-  ((sc/coercer Book book-coercien-matcher) json-book))
+(def coerce-book (scoerce/coercer Book book-coercien-matcher))
 
 ;; NOTE: e.g. schema.coerce has json-coercion-matcher which offers
 ;; existing implementation for e.g. keyword and sets matchers
